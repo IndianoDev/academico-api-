6f91df2d5cfb0bae1b40c9c10d7dd6769f29b8af
unknown <juan.c@iesb.edu.br>
2023-05-15 22:02:22 -0300

    fire base

---
+++ components/Cabecalho.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import React from 'react'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+
+const Cabecalho = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/filmes">academico</Navbar.Brand>
+          <Nav className="me-auto">
+            <Link className='nav-link' href="/Cursos">Cursos</Link>
+            <Link className='nav-link' href="/Diciplinas">Diciplinas</Link>
+            <Link className='nav-link' href="/Alunos">Alunos</Link>
+            <Link className='nav-link' href="/Professores">Professores</Link>
+            <Link className='nav-link' href="/Turmas">Turmas</Link>
+            <Link className='nav-link' href="/Series">Series</Link>
+            <Link className='nav-link' href="/Semestres">Semestres</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
+
+export default Cabecalho
