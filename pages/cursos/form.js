7eea44e36e62f64b04a7a85699119654fbe0b110
unknown <juan.c@iesb.edu.br>
2023-05-18 21:53:25 -0300

    descrição breve do commit01~

--- pages/cursos/form.js
+++ pages/cursos/form.js
@@ -2,41 +2,48 @@ import React, { useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import Pagina from '../../components/Pagina'
 import { useForm } from "react-hook-form";
+import { userRouter } from 'next/router';
 
 const form = () => {
-    const {register, handleSubmit } = useForm()
 
 
-    function salvar(dados){
-        console.log(dados);
-    }
+  const { register, handleSubmit } = useForm()
+
+
+  function salvar(dados) {
+    const cursos = JSON.parse (window.localStorage.getItem ('cursos')) || []
+    cursos.push(dados)
+    window.localStorage.setItem('cursos', JSON.stringify(cursos))
+    
+
+  }
 
   return (
 
-   
 
-    <Pagina titulo='cursos'>
-         <Form>
-      <Form.Group className="mb-3" controlId="nome">
-        <Form.Label>Nome:</Form.Label>
-        <Form.Control type="text" {...register('nome')} /> 
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="duracao">
-        <Form.Label>Duracao:</Form.Label>
-        <Form.Control type="text" {...register('duracao')}/> 
-      </Form.Group>
+    <Pagina titulo='Formulario'>
+      <Form>
+        <Form.Group className="mb-3" controlId="nome">
+          <Form.Label>Nome:</Form.Label>
+          <Form.Control type="text" {...register('nome')} />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="modalidade">
-        <Form.Label>modalidade:</Form.Label>
-        <Form.Control type="text" {...register('modalidade')} /> 
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="duracao">
+          <Form.Label>Duracao:</Form.Label>
+          <Form.Control type="text" {...register('duracao')} />
+        </Form.Group>
 
-    
-      <Button variant="success" onClick={handleSubmit(salvar)}>
-        Salvar
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="modalidade">
+          <Form.Label>modalidade:</Form.Label>
+          <Form.Control type="text" {...register('modalidade')} />
+        </Form.Group>
+
+
+        <Button variant="success" onClick={handleSubmit(salvar)}>
+          Salvar
+        </Button>
+      </Form>
 
     </Pagina>
   )
--- pages/cursos/index.js
+++ pages/cursos/index.js
@@ -1,47 +1,68 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Table } from 'react-bootstrap'
 import Pagina from '../../components/Pagina'
-import { AiOutlinePlus } from 'react-icons/Ai'
+import { AiOutlinePlus  } from 'react-icons/Ai'
+import { BsFillTrash3Fill  } from 'react-icons/Bs'
+
 
 const index = () => {
+
+  const [cursos, setCursos] = useState([])
+
+  useEffect(() => {
+   setCursos(getAll())
+    
+  }, [])
+
+  function getAll(){
+    return JSON.parse(window.localStorage.getItem('cursos')) || []
+  }
+  function excluir(id){
+    const itens = getAll()
+    itens.splice(id, 1)
+    window.localStorage.setItem('cursos', JSON.stringify(itens))
+    setCursos(itens)
+  }
+
+
   return (
     <Pagina titulo='Cursos'>
 
 
-  <Button variant="primary mb-3" >Novo <AiOutlinePlus /> </Button>{' '}
+      <Button href="/cursos/form" variant="primary mb-3" >Novo <AiOutlinePlus /> </Button>{' '}
 
-  
 
-       <Table striped bordered hover>
+
+      <Table striped bordered hover>
         <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <tr>
+
+            <th>
+
+            </th>
+         
+            <th>nome</th>
+            <th>duracao</th>
+            <th>modalidade</th>
+          </tr>
+        </thead>
+        <tbody>
+    {cursos.map((item, i) => (
+        <tr key={i}>
+          <td><Button>< BsFillTrash3Fill onClick={()=>excluir(i)} className='text-danger'/> </Button></td>         
+          <td>{item.nome}</td>
+          <td>{item.duracao}</td>
+          <td>{item.modalidade}</td>
         </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
+  ))}
+
       </tbody>
-    </Table>
+     
+
+    
+      
+      </Table>
     </Pagina>
   )
 }
